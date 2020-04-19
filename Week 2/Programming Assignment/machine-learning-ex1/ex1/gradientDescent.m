f3edee0c13e04d5dfc1a472630d28f149408cbf8
Sare Soltani <sare.soltani96@yahoo.com>
2020-04-19 17:57:35 +0430

    Solved ex1

--- Week 2/Programming Assignment/machine-learning-ex1/ex1/gradientDescent.m
+++ Week 2/Programming Assignment/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+      temp = (X * theta) - y;      
+      theta = theta - ( 1 / m ) * alpha * ( X' * temp);
 
     % ============================================================
 
