7d3b0a2d4f020ab6d740f243cb35d112d8fff15e
Sare Soltani <sare.soltani96@yahoo.com>
2020-05-02 15:55:07 +0430

    Create predict.m

--- Week 3/Programming Assignment/machine-learning-ex2/ex2/predict.m
+++ Week 3/Programming Assignment/machine-learning-ex2/ex2/predict.m
@@ -14,13 +14,14 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
-
-
+  threshold = 0.5;
+  for  i = 1: m , 
+	  if sigmoid( X(i , : )* theta) >= threshold, 
+		  p(i) = 1 ;
+	  else, 
+		  p(i) = 0 ;
+	  end ;
+  end ;
 % =========================================================================
 
 
