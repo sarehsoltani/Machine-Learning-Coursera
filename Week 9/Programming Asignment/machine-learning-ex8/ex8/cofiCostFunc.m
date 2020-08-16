0a376185edf51a4e6180bc0ce38f0e562e3bf172
Sare Soltani <sare.soltani96@yahoo.com>
2020-08-16 23:54:29 +0430

    Solved ex8

--- Week 9/Programming Asignment/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Week 9/Programming Asignment/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -39,19 +39,10 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
+errors = (X * Theta' - Y) .* R;
+J = 1 / 2 * sum(sum(errors .^ 2)) + lambda / 2 * sum(sum(Theta .^ 2)) + lambda / 2 * sum(sum(X .^ 2));
+X_grad = errors * Theta + lambda * X;
+Theta_grad = errors' * X + lambda * Theta;
 
 
 
